06be77a83b612de2ca3f8799df73efb74e6c0253
k-kawamura-internous <k.kawamura@internous.co.jp>
2023-11-27 16:50:49 +0900

    win commit

--- src/main/java/jp/co/internous/framepj/controller/AuthController.java
+++ src/main/java/jp/co/internous/framepj/controller/AuthController.java
@@ -1,5 +1,6 @@
 package jp.co.internous.framepj.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,7 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
 
+import jp.co.internous.framepj.model.domain.MstUser;
 import jp.co.internous.framepj.model.form.UserForm;
+import jp.co.internous.framepj.model.mapper.MstUserMapper;
+import jp.co.internous.framepj.model.mapper.TblCartMapper;
+import jp.co.internous.framepj.model.session.LoginSession;
 
 
 /**
@@ -19,13 +24,16 @@ import jp.co.internous.framepj.model.form.UserForm;
 @RequestMapping("/frameweb/auth")
 public class AuthController {
 	
-	/*
-	 * フィールド定義
-	 */
-	
-	
 	private Gson gson = new Gson();
 	
+	@Autowired
+	private MstUserMapper userMapper;
+	
+	@Autowired
+	private TblCartMapper cartMapper;
+	
+	@Autowired
+	private LoginSession loginSession;
 		
 	/**
 	 * ログイン処理をおこなう
@@ -34,7 +42,31 @@ public class AuthController {
 	 */
 	@PostMapping("/login")
 	public String login(@RequestBody UserForm f) {
+		MstUser user = userMapper.findByUserNameAndPassword(f.getUserName(), f.getPassword());
 		
+		int tmpUserId = loginSession.getTmpUserId();
+		// 仮IDでカート追加されていれば、本ユーザーIDに更新する。
+		if (user != null && tmpUserId != 0) {
+			int count = cartMapper.findCountByUserId(tmpUserId);
+			if (count > 0) {
+				cartMapper.updateUserId(user.getId(), tmpUserId);
+			}
+		}
+		
+		if (user != null) {
+			loginSession.setTmpUserId(0);
+			loginSession.setLogined(true);
+			loginSession.setUserId(user.getId());
+			loginSession.setUserName(user.getUserName());
+			loginSession.setPassword(user.getPassword());
+		} else {
+			loginSession.setLogined(false);
+			loginSession.setUserId(0);
+			loginSession.setUserName(null);
+			loginSession.setPassword(null);
+		}
+		
+		return gson.toJson(user);
 	}
 	
 	/**
@@ -43,16 +75,12 @@ public class AuthController {
 	 */
 	@PostMapping("/logout")
 	public String logout() {
+		loginSession.setTmpUserId(0);
+		loginSession.setLogined(false);
+		loginSession.setUserId(0);
+		loginSession.setUserName(null);
+		loginSession.setPassword(null);
 		
-	}
-
-	/**
-	 * パスワード再設定をおこなう
-	 * @param f ユーザーフォーム
-	 * @return 処理後のメッセージ
-	 */
-	@PostMapping("/resetPassword")
-	public String resetPassword(@RequestBody UserForm f) {
-		
+		return "";
 	}
 }
